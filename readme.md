aabcc28b6acdb1413f940844ee52f5f838a4c401
Alexander Leonov <alex.thunder@tut.by>
2023-07-03 22:00:31 +0300

    add user creating, add env file, add simple readme

---
+++ readme.md
@@ -0,0 +1,3 @@
+### up services
+
+docker-compose --env-file .env up -d
--- webserver/entrypoint.sh
+++ webserver/entrypoint.sh
@@ -2,4 +2,12 @@
 
 airflow db init
 airflow scheduler &
+airflow users create             \
+    --role Admin                 \
+    --username ${AIRFLOW_USER}   \
+    --email ${AIRFLOW_EMAIL}     \
+    --firstname admin            \
+    --lastname admin             \
+    --password ${AIRFLOW_PASSWORD}
+
 exec airflow webserver
